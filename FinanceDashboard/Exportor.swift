a5e23a1810cfbc72cb7d40f489a4019bcba7f402
buhe <bugu1986@126.com>
2023-01-21 10:25:07 +0800

    git-u:update

--- FinanceDashboard/Exportor.swift
+++ FinanceDashboard/Exportor.swift
@@ -32,8 +32,18 @@ struct Pdf: Exportor {
     func export(items: FetchedResults<Item>) -> Data {
         let document = PDFDocument(format: .a4)
       
-        document.add(.contentCenter, text: "Create PDF documents easily.")
-
+        document.add(.contentCenter, text: "Investment Dashboard")
+        
+        let datas = items.map{i in
+            // todo remove dup by same mouthly
+            return ExportData(name: i.name!, categroy: i.categroy!, value: i.value, time: "", unit: i.unit!)}
+        let table = PDFTable(rows: datas.count + 1, columns: 5)
+        datas.enumerated().forEach{
+            index,data in
+            let row = table[row: index]
+            row.content = [data.name,data.categroy,doubleFormat(value: data.value),data.time,data.unit]
+        }
+        document.add(.contentCenter, table: table)
         let generator = PDFGenerator(document: document)
         let data = try! generator.generateData()
 //        let url  = try? generator.generateURL(filename: "Example.pdf")
