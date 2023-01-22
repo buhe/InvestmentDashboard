0aa1caa6ed63c686ae3db0b0ca9435a1d53571a0
buhe <bugu1986@126.com>
2023-01-22 14:20:29 +0800

    git-u:update

--- FinanceDashboard/ContentView.swift
+++ FinanceDashboard/ContentView.swift
@@ -129,6 +129,7 @@ struct ContentView: View {
                 }
             } else {
                 // no biometrics
+                isUnlocked = true
             }
         } else {
             isUnlocked = true
