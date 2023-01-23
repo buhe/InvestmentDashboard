4345a528e8fc80ba7cc7805d6a17db2ec4cf3264
buhe <bugu1986@126.com>
2023-01-23 18:32:15 +0800

    git-u:update

--- FinanceDashboard/TrendView.swift
+++ FinanceDashboard/TrendView.swift
@@ -30,31 +30,54 @@ struct TrendView: View {
     }
     
     var body: some View {
-        Text("Chart")
-            .font(.custom("Avenir", size: 16))
-            .padding(.vertical, 10)
-            .border(width: 1, edges: [.bottom], color: .systemGray)
-        LineChartWrapper(title: "Trend", lineData: viewModel.byTrendValue(items: items))
-//        LineChartView(data: viewModel.byTrendValue(items: items), title: "Trend", form: ChartForm.extraLarge, rateValue: 0)
-        Text("Details")
-            .font(.custom("Avenir", size: 16))
-            .padding(.vertical, 10)
-            .border(width: 1, edges: [.bottom], color: .systemGray)
-        List {
-            ForEach(viewModel.byTrend(items: items)) {
-                i in
-                HStack {
-                    Text(i.name)
-                    Spacer()
-                    Text(itemFormatter.string(from:i.updateDate))
-                    Text(doubleFormat(value: i.value))
-                    Text(i.unit)
+        NavigationStack{
+            Text("Chart")
+                .font(.custom("Avenir", size: 16))
+                .padding(.vertical, 10)
+                .border(width: 1, edges: [.bottom], color: .systemGray)
+                .toolbar{
+                    EditButton()
                 }
-                
+            LineChartWrapper(title: "Trend", lineData: viewModel.byTrendValue(items: items))
+            //        LineChartView(data: viewModel.byTrendValue(items: items), title: "Trend", form: ChartForm.extraLarge, rateValue: 0)
+            Text("Details")
+                .font(.custom("Avenir", size: 16))
+                .padding(.vertical, 10)
+                .border(width: 1, edges: [.bottom], color: .systemGray)
+            List {
+                ForEach(viewModel.byTrend(items: items)) {
+                    i in
+                    HStack {
+                        Text(i.name)
+                        Spacer()
+                        Text(itemFormatter.string(from:i.updateDate))
+                        Text(doubleFormat(value: i.value))
+                        Text(i.unit)
+                    }
+                    
+                }.onDelete {
+                    deleteItems(offsets: $0)
+                }
             }
         }
     }
+    
+    private func deleteItems(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { items[$0] }.forEach(viewContext.delete)
+    
+                do {
+                    try viewContext.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
+            }
+        }
 }
+
 
 //struct TrendView_Previews: PreviewProvider {
 //    static var previews: some View {
