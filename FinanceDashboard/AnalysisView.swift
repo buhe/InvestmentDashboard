85c716e40b82b31ece3bb9aacaa989e206c69802
buhe <bugu1986@126.com>
2023-01-30 13:20:49 +0800

    git-u:update

--- FinanceDashboard/AnalysisView.swift
+++ FinanceDashboard/AnalysisView.swift
@@ -20,34 +20,39 @@ struct AnalysisView: View {
     @Environment(\.colorScheme) private var colorScheme
     
     var body: some View {
-        VStack(alignment: .leading){
-            HStack {
-                Text("Age:")
-                Text(String(overViewModel.model.age))
-                Text("Ratio:")
-                Text(doubleFormat(value: 40))
+        ScrollView{
+            VStack(alignment: .leading){
+                HStack {
+                    Text("Age:")
+                    Text(String(overViewModel.model.age))
+                    Text("Ratio:")
+                    Text(doubleFormat(value: 40))
                     
-            }
-            .font(.title)
-            
-            Divider()
-            Text("Low Risk")
-                .font(.title2)
-            Text("By nature, with low-risk investing, there is less at stake—either in terms of the amount of invested or the significance of the investment to the portfolio. There is also less to gain—either in terms of the potential return or the potential benefit bigger term."
+                }
+                .font(.title)
+                
+                Divider()
+                Text("Low Risk")
+                    .font(.title2)
+                Text("By nature, with low-risk investing, there is less at stake—either in terms of the amount of invested or the significance of the investment to the portfolio. There is also less to gain—either in terms of the potential return or the potential benefit bigger term."
                 )
-            Text("High Risk")
-                .font(.title2)
-                .padding(.top)
-            Text("A high-risk investment is one for which there is either a large percentage chance of loss of capital or under-performance—or a relatively high chance of a devastating loss. The first of these is intuitive, if subjective: If you were told there’s a 50/50 chance that your investment will earn your expected return, you may find that quite risky. If you were told that there is a 95% percent chance that the investment will not earn your expected return, almost everybody will agree that that is risky.")
-            PieChartView(data: [1,2], title: "Rosk", style: colorScheme == .light ? ChartStyle(backgroundColor: Color.white, accentColor: Colors.OrangeStart, secondGradientColor: Colors.OrangeEnd, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: .gray) : ChartStyle(backgroundColor: Color.gray, accentColor: Colors.OrangeStart, secondGradientColor: Colors.OrangeEnd, textColor: Color.white, legendTextColor: Color.white, dropShadowColor: .gray), form: ChartForm.extraLarge)
-                .padding(.top)
-            Text("Summary")
-                .font(.title2)
-                .padding(.top)
-            Text("A high-risk investment is one for which there is either a large percentage chance of loss of capital or under-performance—or a relatively high chance of a devastating loss. The first of these is intuitive, if subjective: If you were told there’s a 50/50 chance that your investment will earn your expected return, you may find that quite risky. If you were told that there is a 95% percent chance that the investment will not earn your expected return, almost everybody will agree that that is risky.")
-            Spacer()
+                .lineLimit(20)
+                Text("High Risk")
+                    .font(.title2)
+                    .padding(.top)
+                Text("A high-risk investment is one for which there is either a large percentage chance of loss of capital or under-performance—or a relatively high chance of a devastating loss. The first of these is intuitive, if subjective: If you were told there’s a 50/50 chance that your investment will earn your expected return, you may find that quite risky. If you were told that there is a 95% percent chance that the investment will not earn your expected return, almost everybody will agree that that is risky.")
+                    .lineLimit(20)
+                PieChartView(data: [1,2], title: "Risk", style: colorScheme == .light ? ChartStyle(backgroundColor: Color.white, accentColor: Colors.OrangeStart, secondGradientColor: Colors.OrangeEnd, textColor: Color.black, legendTextColor: Color.black, dropShadowColor: .gray) : ChartStyle(backgroundColor: Color.gray, accentColor: Colors.OrangeStart, secondGradientColor: Colors.OrangeEnd, textColor: Color.white, legendTextColor: Color.white, dropShadowColor: .gray), form: ChartForm.extraLarge)
+                    .padding(.top)
+                Text("Summary")
+                    .font(.title2)
+                    .padding(.top)
+                Text("A high-risk investment is one for which there is either a large percentage chance of loss of capital or under-performance—or a relatively high chance of a devastating loss. The first of these is intuitive, if subjective: If you were told there’s a 50/50 chance that your investment will earn your expected return, you may find that quite risky. If you were told that there is a 95% percent chance that the investment will not earn your expected return, almost everybody will agree that that is risky.")
+                    .lineLimit(20)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
 //        .backgroundFill(.red)
         
         
